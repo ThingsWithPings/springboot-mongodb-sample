4e0955e8003a0e087189b54ab816a5b50f108277
viraj-directpay <virajdirectpay@gmail.com>
2023-07-18 14:35:22 +0530

    add rest controller advice

--- src/main/java/things/with/pings/mongodb/handler/GlobalExceptionHandler.java
+++ src/main/java/things/with/pings/mongodb/handler/GlobalExceptionHandler.java
@@ -5,13 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import things.with.pings.mongodb.exception.ResourceNotFoundException;
 import things.with.pings.mongodb.model.ErrorDetails;
 
 import java.util.Date;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
